06986a22702c27bdcd958098e89bedd5204a3bb5
Aishwarya Sharma <ishans632@gmail.com>
2018-03-11 22:50:34 +0530

    Add connection event handlers

--- server/server.js
+++ server/server.js
@@ -1,17 +1,32 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
 const app = express();
 
+// Loading the socketIO library
+const socketIO = require('socket.io');
+
+// inbuilt path module making it easy to access directories.
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-console.log(publicPath);
 
+// Using SocketIO
+var server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection',(socket) => {
+  // checks for the connection
+  // The connection is initiated in the public/index.html
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+
+});
 
 app.use(express.static(publicPath));
 
-
-
-
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Listening to port ${port}`);
 });
